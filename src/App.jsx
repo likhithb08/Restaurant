1c05977736f8bb6961521ca5f26d555b3ee4f0eb
likhithb08 <likhithb285@gmail.com>
2025-04-16 14:36:49 +0530

    Frontend

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-amber-300 height-screen flex justify-center items-center'>
+      Restarunt Menue page 
+    </div>
+  )
+}
+
+export default App
